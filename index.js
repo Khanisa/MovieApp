7c46f130becdcb6e2848a259246809d320536f31
Khanisa <y_anisat@ymail.com>
2018-06-30 16:26:35 +0100

    updated index.js file and made some changes in general

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-var API_Key = "93504c920dd227f808629b6700607a8b";
-var movieCards = document.querySelector(".movie-cards") //selecting the .movie-cards class
+var API_KEY = "93504c920dd227f808629b6700607a8b";
+var movieCards = document.querySelector(".movie-cards");
+var dropdown = document.querySelector(".dropdown");
 
-function getLastestMOvies(callback) {
-    fetch("http://api.themoviedb.org/3/discover/movie?api_key=" + API_Key + "&year=2018", {
+function getLatestMovies(callback) {
+    fetch("http://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&year=2018",  {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
         },
         mode: "cors"
     })
-    .then (function(res) {
+    .then(function(res) {
         if(res.status == 200) {
             res.json()
             .then(function(data) {
-                callback(data)
+                callback(data);
             })
         }
     })
 }
 
-getLastestMOvies(function(data) {
-    console.log(data);
+getLatestMovies(function(data) {
     data.results.forEach(function(result) {
-        var card  = creatMovieCard(result);
+        var card = createMovieCard(result);
         movieCards.appendChild(card);
     })
 });
 
-function creatMovieCard (data) {
+function createMovieCard(data) {
     var li = document.createElement("li"); //creating li
-    li.classList.add("movie-card") //add class to li
-    var img = document.createElement("img") ///creating img
+    li.classList.add("movie-card"); //add class to li
+    var img = document.createElement("img"); ///creating img
     img.classList.add("image"); //adding image class
-    img.setAttribute("src", "http://image.tmdb.org/t/p/w500" + data.poster_path ); //setting attribute for the img task
+    img.setAttribute("src", "http://image.tmdb.org/t/p/w500" + data.poster_path); //setting attribute for the img
     li.appendChild(img);
     var div = document.createElement("div"); //creating div
     div.classList.add("img-detail"); //adding
@@ -48,32 +48,71 @@ function creatMovieCard (data) {
     return li;
 }
 
-// function getActionMovies(callback) {
-//     fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_Key + "&with_genres=18", {
-//         method: "GET",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         mode: "cors"
-//     })
-//     .then (function(res) {
-//         if(res.status == 200) {
-//             res.json()
-//             .then(function(data) {
-//                 callback(data)
-//             })
-//         }
-//     })
-// }
+function getMovieGenres(callback) {
+    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY, {
+        method: "GET",
+        mode: "cors"
+    })
+    .then(function(res) {
+        if(res.status === 200) {
+            res.json()
+            .then(function(data) {
+                callback(data.genres);
+            })
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+}
 
-// getActionMovies(function(data) {
-//     console.log(data);
-//     data.results.forEach(function(result) {
-//         var card  = creatMovieCard(result);
-//         movieCards.appendChild(card);
-//     })
-// });
+function createDropdownOption(data) {
+    var li = document.createElement("li");
+    li.classList.add("option");
+    li.setAttribute("id", data.id);
+    li.innerText = data.name;
+    li.addEventListener("click", function() {
+        var id = this.getAttribute("id");
+        getMoviesByGenre(id, function(movies) {
+            populateMovies(movies);
+        })
+    })
+    return li;
+}
 
+function populateDropdown() {
+    getMovieGenres(function(genres) {
+        genres.forEach(function(data) {
+            var dropdownOption = createDropdownOption(data);
+            dropdown.appendChild(dropdownOption);
+        });
+    })
+}
 
-// var className = document.getElementsByClassName("about_page")
-// className.addEventListener("click", getActionMovies);
+populateDropdown();
+
+function getMoviesByGenre(id, callback) {
+    fetch("https://api.themoviedb.org/3/genre/" + id + "/movies?api_key=" + API_KEY, {
+        method: "GET",
+        mode: "cors"
+    })
+    .then(function(res) {
+        if(res.status === 200) {
+            res.json()
+            .then(function(data) {
+                callback(data.results);
+            })
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+function populateMovies(data) {
+    movieCards.innerHTML = "";
+    data.forEach(function(movie) {
+        var movieCard = createMovieCard(movie);
+        movieCards.appendChild(movieCard);
+    })
+}
